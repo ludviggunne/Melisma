41d2d22856516d242e698785be138884fa2f7f6b
Ludvig Gunne Lindström <87268743+ludviggunne@users.noreply.github.com>
2022-01-23 18:51:58 +0100

    Removed SandBoxApplication::Run
    This is now a final function in melisma::Application.

--- SandBox/src/SandboxApp.cpp
+++ SandBox/src/SandboxApp.cpp
@@ -8,22 +8,6 @@ class SandboxApp : public Application {
 public:
 	SandboxApp() : Application() {}
 	virtual ~SandboxApp() override {}
-
-	Renderer2D renderer;
-
-	virtual void Run() override {
-
-		while (m_Running) {
-
-			renderer.Clear();
-			renderer.BeginScene();
-
-			renderer.EndScene();
-
-			m_LayerStack.OnUpdate(DeltaTime()); 
-			m_Window->OnUpdate(DeltaTime());
-		}
-	}
 };
 
 Application *CreateApplication() {
