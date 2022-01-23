bbbbaa95526a51c0f74100400190061630f918eb
Ludvig Gunne Lindström <87268743+ludviggunne@users.noreply.github.com>
2022-01-23 21:10:21 +0100

    Added Viewport constructors.

--- Melisma/include/melisma/rendering/renderer2D.h
+++ Melisma/include/melisma/rendering/renderer2D.h
@@ -14,6 +14,10 @@
 namespace melisma {
 
 	struct Viewport {
+		Viewport() : X(0), Y(0), Width(800), Height(600) {}
+		Viewport(int x, int y, int width, int height)
+			: X(x), Y(y), Width(width), Height(height) {}
+
 		int X = 0;
 		int Y = 0;
 		int Width = 100;
