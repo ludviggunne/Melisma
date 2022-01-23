d987c6b6b9e2da5dd8dd80783cd23ae090127263
Ludvig Gunne Lindström <87268743+ludviggunne@users.noreply.github.com>
2022-01-23 21:00:44 +0100

    Testing rendering from layer.

--- SandBox/src/SandboxApp.cpp
+++ SandBox/src/SandboxApp.cpp
@@ -4,10 +4,24 @@ using namespace melisma;
 
 #include <format>
 
+class RenderLayer : public Layer {
+public:
+
+	virtual void OnUpdate(const DeltaTime&) override {
+		Renderer2D::Clear();
+		Renderer2D::BeginScene();
+		Renderer2D::DrawQuad({ -.5, -.5 }, { 1, 1 }, { 1, 0, 0, 1 });
+		Renderer2D::EndScene();
+	}
+
+private:
+};
+
 class SandboxApp : public Application {
 public:
-	SandboxApp() : Application() {}
-	virtual ~SandboxApp() override {}
+	SandboxApp() : Application() {
+		PushLayerTop(Ref<Layer>(new RenderLayer));
+	}
 };
 
 Application *CreateApplication() {
