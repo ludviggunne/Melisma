0876cba4e39c751c5931b8ef61e81583e02cd91b
Ludvig Gunne Lindström <87268743+ludviggunne@users.noreply.github.com>
2022-01-24 00:00:26 +0100

    Replaced Ref/Scope with standard library wrappers.
    From TheCherno/Hazel.

--- Melisma/include/melisma/pointers.h
+++ Melisma/include/melisma/pointers.h
@@ -13,142 +13,19 @@
 namespace melisma {
 
 	template<typename T>
-	class Ref { // Melisma Warning: Not thread safe ?
-	public:
-		Ref() : m_Instance(nullptr), m_Refs(nullptr) {}
+	using Scope = std::unique_ptr<T>;
+	template<typename T, typename ... Args>
+	constexpr Scope<T> CreateScope(Args&& ... args)
+	{
+		return std::make_unique<T>(std::forward<Args>(args)...);
+	}
 
-		template<typename... ArgT>
-		static Ref Create(ArgT... args)
-		{
-			Ref ref; 
+	template<typename T>
+	using Ref = std::shared_ptr<T>;
+	template<typename T, typename ... Args>
+	constexpr Ref<T> CreateRef(Args&& ... args)
+	{
+		return std::make_shared<T>(std::forward<Args>(args)...);
+	}
 
-			ref.m_Instance = new T(args...);
-			ref.m_Refs = new unsigned int;
-			*(ref.m_Refs) = 1;
-
-			mlLogRef("Created");
-
-			return ref;
-		}
-
-		Ref(T *&&ptr) : Ref()
-		{
-			m_Instance = ptr;
-			m_Refs     = new unsigned int;
-			*m_Refs    = 1;
-		}
-
-
-		void Clear() {
-
-			(*m_Refs)--;
-
-			mlLogRef("Destructed, refs = " << *m_Refs);
-
-			if (*m_Refs == 0) {
-				delete m_Refs;
-				delete m_Instance;
-
-				mlLogRef("No references left, deleting data.");
-			}
-
-			m_Refs = nullptr;
-			m_Instance = nullptr;
-		}
-
-
-
-		~Ref() 
-		{
-			if (m_Refs)
-			{
-				(*m_Refs)--;
-
-				mlLogRef("Destructed, refs = " << *m_Refs);
-
-				if (*m_Refs == 0) {
-					delete m_Refs;
-					delete m_Instance;
-
-					mlLogRef("No references left, deleting data.");
-				}
-			}
-		}
-
-
-
-		Ref(const Ref &ref) : Ref()
-		{
-			m_Instance = ref.m_Instance;
-			m_Refs     = ref.m_Refs;
-
-			(*m_Refs)++;
-			mlLogRef("lvalue copy, refs = " << *m_Refs);
-		}
-
-		Ref(Ref &&ref) noexcept : Ref()
-		{
-			m_Instance = ref.m_Instance;
-			m_Refs     = ref.m_Refs;
-
-			(*m_Refs)++;
-			mlLogRef("rvalue copy, refs = " << *m_Refs);
-		}
-
-		Ref &operator=(const Ref &ref)
-		{
-			if (*this != ref) {
-				m_Instance = ref.m_Instance;
-				m_Refs     = ref.m_Refs;
-
-				(*m_Refs)++;
-				mlLogRef("lvalue assign, refs = " << *m_Refs);
-			}
-
-			return *this;
-		}
-
-		Ref &operator=(Ref &&ref) {
-			if (*this != ref) {
-				if (m_Instance)
-				{
-					(*m_Refs)--;
-					mlLogRef("Reassigned, refs = " << *m_Refs);
-
-					if (*m_Refs == 0) {
-						delete m_Refs;
-						delete m_Instance;
-
-						mlLogRef("No references left, deleting data.");
-					}
-				}
-
-				m_Instance = ref.m_Instance;
-				m_Refs     = ref.m_Refs;
-
-				(*m_Refs)++;
-				mlLogRef("rvalue assign, refs = " << *m_Refs);
-			}
-
-			return *this;
-		}
-
-		T *operator->() const {
-			return m_Instance;
-		}
-
-		bool operator==(const Ref &ref) {
-			return m_Instance == ref.m_Instance;
-		}
-
-		bool operator!=(const Ref &ref) {
-			return m_Instance != ref.m_Instance;
-		}
-
-	private:
-		T *m_Instance;
-		unsigned int *m_Refs;
-	};
-
-	// Melisma Todo: Implement Scope<T>
 }
