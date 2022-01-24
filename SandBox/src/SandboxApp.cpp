c35eddcc73394aaaf5a68820af133f2ccddcd24e
Ludvig Gunne Lindström <87268743+ludviggunne@users.noreply.github.com>
2022-01-24 13:12:52 +0100

    SandBox update.

--- SandBox/src/SandboxApp.cpp
+++ SandBox/src/SandboxApp.cpp
@@ -2,63 +2,110 @@
 
 using namespace melisma;
 
+#define SCR_WIDTH 800
+#define SCR_HEIGHT 800
+
+#include <vector>
+
 class RenderLayer : public Layer {
 public:
 	virtual void OnAttach() override {
+
+		// Load textures
+		
 		TextureSpecification spec;
 		spec.colorSpace = ColorSpace::RGB;
-		m_Texture = CreateRef<Texture>("textures/erling.jpg", spec);
 
-		Renderer2D::SetViewport(Viewport(0, 0, 800, 600));
-		m_Camera = Camera(glm::ortho(0.0f, (float)800, 0.0f, (float)600, -1.0f, 100.0f));
+		m_Textures.push_back(CreateRef<Texture>("textures/erling.jpg",  spec));
+		m_Textures.push_back(CreateRef<Texture>("textures/matisse.jpg", spec));
+		spec.colorSpace = ColorSpace::RGBA;
+		m_Textures.push_back(CreateRef<Texture>("textures/Tree.png", spec));
+		m_Textures.push_back(CreateRef<Texture>("textures/map.png", spec));
 
-		m_Pos = glm::vec2(0.0f);
+		m_ActiveTexture = m_Textures.begin();
+
+		// Init viewport
+		Renderer2D::SetViewport(Viewport(0, 0, SCR_WIDTH, SCR_HEIGHT));
+		m_Camera = Camera(glm::ortho(0.0f, (float)SCR_WIDTH, 0.0f, (float)SCR_HEIGHT, -1.0f, 100.0f));
+
+		m_Scale = 1.0f;
 	}
+
 
 	virtual void OnUpdate(const DeltaTime&) override {
 
-		static float speed = 1.0f;
-		m_Pos.x += speed * ((float)IsKeyPressed(KeyCode::Right) - (float)IsKeyPressed(KeyCode::Left));
+		// COnfigure docking
+		auto right_x = Application::Instance->GetWindow()->GetWidth()  - m_Scale * (*m_ActiveTexture)->Width();
+		auto top_y   = Application::Instance->GetWindow()->GetHeight() - m_Scale * (*m_ActiveTexture)->Height();
 
-		auto m = glm::translate(glm::mat4(1.0f), glm::vec3(m_Pos, 0.0f)) * m_Camera.GetProjection();
-		static Camera cam(m);
-
-		Renderer2D::Clear();
-		Renderer2D::BeginScene(cam);
-		Renderer2D::DrawTexturedQuad({ 0, 0 }, m_Texture);
+		// Renderer textures
+		Renderer2D::Clear(glm::vec4(0.3f, 0.2f, 0.2f, 1.0f));
+		Renderer2D::BeginScene(m_Camera);
+		Renderer2D::DrawTexturedQuad({ 0,		0		}, *m_ActiveTexture, m_Scale);
+		Renderer2D::DrawTexturedQuad({ right_x, 0		}, *m_ActiveTexture, m_Scale);
+		Renderer2D::DrawTexturedQuad({ 0,		top_y	}, *m_ActiveTexture, m_Scale);
+		Renderer2D::DrawTexturedQuad({ right_x, top_y	}, *m_ActiveTexture, m_Scale);
 		Renderer2D::EndScene();
+
+		// Poll input
+		if (IsKeyPressed(KeyCode::Up)) m_Scale *= 1.02f;
+		if (IsKeyPressed(KeyCode::Down))  m_Scale /= 1.02f;
 	}
 
+
 	virtual bool OnWindowResize(WindowResizeEvent &e) override {
+
+		// Update viewport
 		Renderer2D::SetViewport(Viewport(0, 0, e.Width, e.Height));
 		m_Camera = Camera(glm::ortho(0.0f, (float)e.Width, 0.0f, (float)e.Height, -1.0f, 100.0f));
 
 		return false;
 	}
 
+
 	virtual bool OnKeyPressed(KeyPressedEvent &e) override {
 		mlLog("Key pressed: " << (int)e.GetKeyCode());
 
+		// Exit
 		if (e.GetKeyCode() == KeyCode::Escape) {
 			WindowCloseEvent we;
 			Application::Instance->OnEvent((Event&)we);
 		}
+
+		// Cycle through textures
+		if (e.GetKeyCode() == KeyCode::Space) {
+			m_ActiveTexture++;
+
+			if (m_ActiveTexture == m_Textures.end())
+				m_ActiveTexture = m_Textures.begin();
+		}
+
 		return false;
 	}
 
+
 private:
-	Ref<Texture> m_Texture;
+	std::vector<Ref<Texture>> m_Textures;
+	std::vector<Ref<Texture>>::const_iterator m_ActiveTexture;
+
 	Camera m_Camera;
-	glm::vec2 m_Pos;
+	float m_Scale;
 };
+
+
 
 
 class SandboxApp : public Application {
 public:
 	SandboxApp() : Application() {
 		PushLayerTop(Ref<Layer>(new RenderLayer));
+
+		m_Window->Maximize();
 	}
 };
+
+
+
 
 Application *CreateApplication() {
 	return new SandboxApp;
