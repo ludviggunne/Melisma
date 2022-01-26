32c286c39531c7c0e03cf533ebb4ca7ada85c10e
Ludvig Gunne Lindström <87268743+ludviggunne@users.noreply.github.com>
2022-01-26 01:05:41 +0100

    Simplyfied DeltaTime declaration.

--- Melisma/include/melisma/delta_time.h
+++ Melisma/include/melisma/delta_time.h
@@ -3,15 +3,6 @@
 
 namespace melisma {
 
-	class melismaAPI DeltaTime {
-	public:
-		DeltaTime() : m_Time(1.0) {}
-
-		template<typename T>
-		inline double operator *(const T &value) { return m_Time * value; }
-
-	private:
-		double m_Time;
-	};
+	using DeltaTime = float;
 
 }
