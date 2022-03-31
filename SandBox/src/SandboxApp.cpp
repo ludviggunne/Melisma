889306ca2f49df2f79cf6306bb03ae26c97df1a0
Ludvig Gunne Lindström <87268743+ludviggunne@users.noreply.github.com>
2022-03-31 21:39:37 +0200

    Update SandboxApp.cpp

--- SandBox/src/SandboxApp.cpp
+++ SandBox/src/SandboxApp.cpp
@@ -2,85 +2,84 @@
 
 using namespace melisma;
 
-#include <vector>
-#include <filesystem>
-#include <format>
-
 class ExampleLayer : public Layer {
 public:
 	virtual void OnAttach() override {
 
-		// Texture specification
-		TextureSpecification tspec;
-		tspec.hTiles = 4;
-		tspec.vTiles = 4;
-		m_Texture = CreateRef<Texture>("textures/map.jpg", tspec);
+		auto viewport = Application::Instance->GetWindow()->GetViewport();
 
-		m_UV = 0;
-
-		CalculateView(
-			Application::Instance->GetWindow()->GetWidth(),
-			Application::Instance->GetWindow()->GetHeight());
+		m_PlayerPos = glm::vec2(508.0f);
+		m_Camera = CreateScope<ScreenCoordCamera>(viewport, m_PlayerPos, 1.0f);
+		Renderer2D::SetViewport(viewport);
 	}
 
-	virtual void OnRender() override {
-		Renderer2D::Clear(glm::vec4(0.2f));
+	virtual void OnUpdate(const DeltaTime &dt) override {
 
-		Renderer2D::BeginScene(m_Camera);
-		Renderer2D::DrawTexturedQuad(m_ImagePos, m_Texture, m_ImageSize, m_UV % 4, m_UV / 4);
+		const float player_base_speed = 200.0f;
+		float player_speed = player_base_speed * (!IsKeyPressed(KeyCode::LeftShift) ? 1.0f : 0.5f);
+
+		if (IsKeyPressed(KeyCode::Right)) m_PlayerPos.x += player_speed * dt;
+		if (IsKeyPressed(KeyCode::Left)) m_PlayerPos.x  -= player_speed * dt;
+		if (IsKeyPressed(KeyCode::Down)) m_PlayerPos.y  -= player_speed * dt;
+		if (IsKeyPressed(KeyCode::Up)) m_PlayerPos.y    += player_speed * dt;
+
+		const float attraction = .015f;
+		float distance = glm::length(m_Camera->Position - m_PlayerPos);
+
+		if (distance > 50.0f * dt)
+		{
+			m_Camera->Position += (m_PlayerPos - m_Camera->Position) * (distance + 50.0f) * attraction * dt;
+		}
+		else {
+			m_Camera->Position = m_PlayerPos;
+		}
+	}
+
+	virtual void OnRender() {
+		Renderer2D::Clear(glm::vec4(.2f));
+		Renderer2D::BeginScene((Camera&) *m_Camera);
+
+		for(int i = 0; i < 50; i++)
+			for (int j = 0; j < 50; j++)
+				Renderer2D::DrawQuad(
+          glm::vec2(20 * i, 20 * j), 
+          glm::vec2(16, 16), 
+          glm::vec4(i / 50.0f, 0.5f, j / 50.0f, 1.0f));
+		
+		const int player_size = 200.0f;
+		Renderer2D::DrawQuad(
+			m_PlayerPos - glm::vec2(player_size / 2),
+			glm::vec2(player_size),
+			glm::vec4(0.2f, 0.6f, 0.8f, 1.0f));
+
 		Renderer2D::EndScene();
 	}
 
-	virtual bool OnWindowResize(WindowResizeEvent &e) {
-		CalculateView(e.Width, e.Height);
-		mlLog("WindowResizeEvent: " << e.Width << ", " << e.Height);
-		return false;
-	}
+	virtual bool OnWindowResize(WindowResizeEvent &e) override {
 
-	virtual bool OnKeyPressed(KeyPressedEvent &e) override {
+		auto viewport = Application::Instance->GetWindow()->GetViewport();
 
-		if (e.IsRepeated()) return false;
-
-		switch (e.GetKeyCode()) {
-		case KeyCode::Right:
-			m_UV++;
-			break;
-
-		case KeyCode::Left:
-			m_UV--;
-			break;
-		}
-
-		CalculateView(
-			Application::Instance->GetWindow()->GetWidth(),
-			Application::Instance->GetWindow()->GetHeight());
+		m_Camera->Viewport = viewport;
+		Renderer2D::SetViewport(viewport);
 
 		return true;
 	}
 
-	void CalculateView(int window_width, int window_height) {
-		float x_ratio = window_width / static_cast<float>(m_Texture->Width());
-		float y_ratio = window_height / static_cast<float>(m_Texture->Height());
-		
-		if (x_ratio > y_ratio) {
-			m_ImageSize = y_ratio;
-			m_ImagePos = glm::vec2(.5f * (window_width - m_ImageSize * m_Texture->Width()), 0);
-		}
-		else {
-			m_ImageSize = x_ratio;
-			m_ImagePos = glm::vec2(0, .5f * (window_height - m_ImageSize * m_Texture->Height()));
-		}
+	virtual bool OnKeyPressed(KeyPressedEvent &e) override {
+		if (e.GetKeyCode() == KeyCode::Escape)
+		{
+			WindowCloseEvent e;
+			Application::Instance->OnEvent(e);
 
-		Renderer2D::SetViewport(Viewport(0, 0, window_width, window_height));
-		m_Camera = Camera(glm::ortho(0.0f, (float)window_width, 0.0f, (float)window_height, -1.0f, 10.0f));
+			return true;
+		}
+		
+		return false;
 	}
 
 private:
-	glm::vec2 m_ImagePos;
-	float m_ImageSize;
-	int m_UV;
-	Ref<Texture> m_Texture;
-	Camera m_Camera;
+	Scope<ScreenCoordCamera> m_Camera;
+	glm::vec2 m_PlayerPos;
 };
 
 class SandboxApp : public Application {
@@ -88,6 +87,7 @@ public:
 	using Application::Application;
 
 	virtual void Init() override {
+		m_Window->SetSize(1000, 1000);
 		PushLayerTop(CreateRef<ExampleLayer>());
 	}
 };
